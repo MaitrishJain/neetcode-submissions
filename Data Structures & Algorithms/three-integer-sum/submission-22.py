7f2086e649d655d97a6d556dee4cbaea16c04b88
MaitrishJain <79186580+MaitrishJain@users.noreply.github.com>
2026-07-28 12:53:40 +0530

    Add: three-integer-sum - submission-22

---
+++ Data Structures & Algorithms/three-integer-sum/submission-22.py
@@ -0,0 +1,23 @@
+from collections import Counter
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        start = 0
+        end = len(nums) - 1
+        frequency_map = Counter(nums)
+        nums.sort()
+        results = []
+        print(frequency_map)
+        print(nums)
+        while start < end:
+            frequency_map[nums[start]] -= 1
+            frequency_map[nums[end]] -= 1
+            counter = -(nums[start] + nums[end])
+            if counter in frequency_map and frequency_map[counter] != 0 and sorted([nums[start], nums[end], counter]) not in results:
+                results.append(sorted([nums[start], nums[end], counter]))
+            frequency_map[nums[start]] += 1
+            frequency_map[nums[end]] += 1
+            if nums[start] + nums[end] < 0:
+                start += 1
+            else:
+                end -= 1     
+        return results      
